501bc369f6dcad461da9a0f34e0582023fe7d30b
Parambula <juan.bolanos@neoris.com>
2021-04-12 17:09:28 -0500

    Init Commit -Deleted defaul files -Added tailwind to the project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,98 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import "tailwindcss/tailwind.css";
+import Barrita from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+import Cabezal from "../components/cabezal/cabezal";
+import Titulo from "../components/title/title";
+import Parrafo from "../components/parrafo/parrafo";
+import Fila from "../components/fila/fila";
+import Banner from "../components/banner/banner";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <Cabezal title="BedRoom Studio" />
+      <Barrita />
+      <Banner />
+      <Titulo text="BedRoom Studio" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <Fila
+        ParrafoTitle="Historia"
+        ParrafoBody="BedRoom Studio es un estudio de grabación ubicado en Santa Catarina, N.L.
+            liderado por Paulo Arambula. El estudio se construyó y abrió sus puertas en
+            2020, y desde entonces se ha mantenido activo capturando y
+            produciendo discos principalmente de géneros como
+            metal e indie, siguiendo siempre nuestra
+            filosofía de mantener la música lo más real y genuina posible."
+        Image="/bateria.jpg"
+      />
+
+      <div className="grid grid-cols-1 py-6 md:grid-cols-2 gap-16 px-6 lg:px-12">
+        <div>
+          <Parrafo
+            MainText="El Estudio"
+            SecundaryText="Con la mejor calidad de equipo tanto análogo como digital, el cual
+            actualizamos constantemente, logramos un buen sonido de manera
+            rápida y a un precio razonable. En BedRoom Studio podrás encontrar marcas
+            como Universal Audio, API, Shadow Hills, Daking, ALTEC, RCA, Rupert
+            Neve, dbx, Empirical Labs, Tascam, SSL, Oktava, ADAM Audio, Yamaha,
+            Audio Technica, Shure, Electro Voice, entre otros."
+          />
+          <p>
+            El cuarto de estudio está profesionalmente tratado para aprovechar
+            su acústica y que el sonido pueda ser registrado en él de la mejor
+            manera. También contamos con un iso booth / cuarto seco, donde el
+            eco y la reverberación es mínima para poder conseguir un sonido más
+            claro y limpio, para tener la opción de tratarlo mejor en post
+            producción.
+          </p>
+          <br />
+          <p>
+            Trabajamos para ofrecer a nuestros clientes los mejores
+            instrumentos, todos ellos a su disposición. Tenemos una selección de
+            baterías y tambores, platillos, tarolas, guitarras, bajos; y una
+            variedad amplificadores de todo tipo para escoger lo mejor según el
+            sonido que busquen capturar para su estilo musical, desde artículos
+            vintage hasta modelos actuales. Contamos con una variedad de
+            teclados y sintetizadores Korg, Mellotron, Roland, ARP, Rhodes así
+            como sistemas modulares para poder experimentar y arreglar sus
+            composiciones.
+          </p>
+          <br />
+          <p>
+            También contamos con una amplia gama de efectos, desde análogos a
+            digitales, delays y echos análogos como Roland Space Echo, Maestreo
+            Echoplex, Premier reverb, Thermionic Culture Vulture, Eventide H3000
+            y una amplia selección de pedales desde Earthquaker, Death by Audio,
+            Analogman, Moog, Shin Ei, Electroharmonix, MXR, Ibanez, Hologram
+            Electronics, ZVEX, BOSS, entre otros.
+          </p>
+        </div>
+        <img src="/synths.jpg"></img>
+      </div>
+
+      <Fila
+        ParrafoTitle="Servicios"
+        ParrafoBody="Nuestros servicios incluyen grabación, mezcla, master y producción.
+        Todo de la manera más profesional, rápida y eficiente para lograr
+        que nuestros clientes salgan satisfechos después de trabajar con
+        nosotros."
+        Image="/daw.jpg"
+      />
+
+      <br />
+
+      <div>
+        <h1 className="mb-6 font-bold text-3xl text-center leading-normal">
+          <span>Síguenos en nuestras redes sociales</span>
+          <hr className="mt-4 w-20 border-solid border-b-2 border-black m-auto" />
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+        <div className="flex items-center content-center justify-center">
+          <a href="https://www.google.com" target="_blank">
+            <img className="h-12" src="/facebook-icon.svg" />
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
